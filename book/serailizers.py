c39b3e011fb3cf8323107a3759a9bcf8853f073f
aliqnbri <aliqnbri1998@gmail.com>
2024-07-28 20:13:06 +0330

    Style: book Serailizer

--- book/serailizers.py
+++ book/serailizers.py
@@ -1,16 +1,12 @@
 from rest_framework import serializers
 from .models import Book
-from django.urls import reverse
 from django.urls import reverse_lazy
+from typing import Any, Dict, List
 
 
 class SimpleReviewSerializer(serializers.Serializer):
     user = serializers.CharField(max_length=200)
     rating = serializers.IntegerField()
-
-
-
-
 
 
 class BookSerializer(serializers.Serializer):
@@ -19,45 +15,37 @@ class BookSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=200)
     author = serializers.CharField(max_length=200)
     genre = serializers.CharField(max_length=200)
-    # reviews = serializers.SerializerMethodField()
-
-
 
     def update(self, instance, validated_data):
-        updated_instance:dict = Book.update(instance['id'], **validated_data)
+        updated_instance: dict = Book.update(instance['id'], **validated_data)
         updated_instance = Book.get_book_by_reviews(updated_instance['id'])
         return updated_instance
 
-
-    def get_detail_url(self, instance):
+    def get_detail_url(self, instance: Book) -> str:
         request = self.context.get('request')
-        # return [request.build_absolute_uri(reverse_lazy('book:book-detail', kwargs={'id': item['id']})) for item in instance]
-        return request.build_absolute_uri(reverse_lazy('book:book-detail', kwargs={'id': instance['id']}))    
-    
-    def get_list_url(self):
+        return request.build_absolute_uri(reverse_lazy('book:book-detail', kwargs={'id': instance.id}))
+
+    def get_list_url(self) -> str:
         request = self.context.get('request')
         return request.build_absolute_uri(reverse_lazy('book:book-list'))
 
+    def get_reviews(self, instance: Book) -> List[Dict[str, Any]]:
+        return [SimpleReviewSerializer(review).data for review in instance.reviews]
 
-    def get_reviews(self, instance):
-            return [SimpleReviewSerializer(review).data for review in instance['reviews']]
-
-
-    def to_representation(self, instance):
+    def to_representation(self, instance: Book) -> Dict[str, Any]:
         representation = super().to_representation(instance)
         request = self.context.get('request')
         view = self.context.get('view')
 
         match view.action:
-            case 'list'| 'destroy':
+            case 'list' | 'destroy':
                 representation.pop('reviews', None)
             case 'retrieve' | 'create':
                 representation.pop('detail_url')
-                representation = {'list_url': self.get_list_url(), **representation}
+                representation = {
+                    'list_url': self.get_list_url(), **representation}
                 representation['reviews'] = self.get_reviews(instance)
             case 'update':
                 representation['reviews'] = self.get_reviews(instance)
 
-
         return representation
-
--- book/urls.py
+++ book/urls.py
@@ -5,6 +5,7 @@ from .views import BookViewSet
 app_name = 'book'
 router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='book')
+router.register(r'users',user )
 
 urlpatterns = [
     path('', include(router.urls)),
